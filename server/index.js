99431347ea87cc6fd11b9d22b339c0258253d8b9
SRV30 <142303159+SRV30@users.noreply.github.com>
2025-01-25 22:02:17 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -5,28 +5,40 @@ import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 // Routes
 import hotelRoutes from "./routes/hotels.js";
-import vehicleRoutes from "./routes/vechicles.js";  
+import vehicleRoutes from "./routes/vechicles.js";
 import regionRoutes from "./routes/regions.js";
 
 dotenv.config();
 
-
-const username=process.env.DB_USERNAME;
-const password=process.env.PASSWORD;    
-const url=`mongodb+srv://${username}:${password}@cluster0.wzf24.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const username = process.env.DB_USERNAME;
+const password = process.env.PASSWORD;
+const url = `mongodb+srv://${username}:${password}@cluster0.wzf24.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Connect to DB
 connectDB(url);
 
 // Initialize Express
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://hotel-zjb6.vercel.app",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.json({
-      message: "Server is running " + PORT,
-    });
+  res.json({
+    message: "Server is running " + PORT,
   });
+});
 
 // API Routes
 app.use("/api", hotelRoutes);
